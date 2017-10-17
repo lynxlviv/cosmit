45e10b22635f4bf604cc3e41e8d089b0917a53f9
lynxlviv <user@example.comlynxlviv@ukr.net>
2017-10-17 03:08:15 +0300

    added bootstrap

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     function bodyFixHeight() {
+        var vh = $(window).height();
         $("#header").css("height", ($(window).height()) - 100 + "px");
-                $("section").css("min-height", ($(window).height()) - 100 + "px");
+        if( vh < 970){
+                $("section").css("min-height", ($(window).height()) - 100 + "px")};
     };
     bodyFixHeight();
     $(window).resize(function () {
@@ -10,37 +12,37 @@ $(document).ready(function () {
         $("#header").css("min-height", vh - 100 + "px");
         bodyFixHeight();
     });
-    $("a.toSection1").click(function () {
+    $("li.toSection1").click(function () {
         $("html,body").animate({
             scrollTop: $(".section1").offset().top - 100
         }, 1000);
         return false;
     });
-    $("a.toSection2").click(function () {
+    $("li.toSection2").click(function () {
         $("html,body").animate({
             scrollTop: $(".section2").offset().top - 100
         }, 1000);
         return false;
     });
-    $("a.toSection3").click(function () {
+    $("li.toSection3").click(function () {
         $("html,body").animate({
             scrollTop: $(".section3").offset().top - 100
         }, 1000);
         return false;
     });
-    $("a.toSection4").click(function () {
+    $("li.toSection4").click(function () {
         $("html,body").animate({
             scrollTop: $(".section4").offset().top - 100
         }, 1000);
         return false;
     });
-    $("a.toSection5").click(function () {
+    $("li.toSection5").click(function () {
         $("html,body").animate({
             scrollTop: $(".section5").offset().top - 100
         }, 1000);
         return false;
     });
-    $("a.toSection6").click(function () {
+    $("li.toSection6").click(function () {
         $("html,body").animate({
             scrollTop: $(".section6").offset().top - 100
         }, 1000);
@@ -50,16 +52,16 @@ $(document).ready(function () {
     function up() {
         var section2Top = ($(".section1").offset().top) / 3;
         if ($(document).scrollTop() >= section2Top) {
-            $(".upBtn").css("display", "block")
+            $(".up-btn").css("display", "block")
         }
         else if ($(document).scrollTop() < section2Top) {
-            $(".upBtn").css("display", "none")
+            $(".up-btn").css("display", "none")
         }
     };
     $(document).scroll(function () {
         up();
     })
-    $(".upBtn").click(function () {
+    $(".up-btn").click(function () {
         $("html,body").animate({
             scrollTop: 0
         }, 1000);
